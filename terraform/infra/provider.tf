c53373495f216c33d4fb4bc83e6c07b8b7d0f97b
abdelhegazi <abdo.hegazy@gmail.com>
2018-03-25 18:34:30 +0100

    removing key name

--- terraform/infra/provider.tf
+++ terraform/infra/provider.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
+/*
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config {
+    bucket = "${data.aws_caller_identity.current.account_id}-eu-west-1-terraform-state"
+    key    = "utas/flask.tfstate"
+    region = "eu-west-1"
+  }
+}*/
+
